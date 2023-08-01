4cf03ef537ea7a1211aa39d90dcb1d7e75d88eec
Elena <elenarosamail@gmail.com>
2023-08-01 15:39:49 -0700

    add Test 1, constructor

---
+++ __tests__/age.test.js
@@ -0,0 +1,10 @@
+import Age from '../src/age.js';
+
+describe('Age', () => {
+    test("returns age for earth years", () => {
+
+        const newAge = new Age(47)
+        expect(newAge.years).toBe(47);
+
+    });
+});
---
+++ src/age.js
@@ -0,0 +1,18 @@
+export default class Age {
+    constructor(years) {
+        this.years = years;
+    }
+    getMercuryYears() {
+        return this.years / .24;
+    }
+    getVenusYears() {
+        return this.years / .62;
+    }
+    getMarsYears() {
+        return this.years / 1.88;
+    }
+    getJupiterYears() {
+        return this.years / 11.86;
+    }
+
+}
